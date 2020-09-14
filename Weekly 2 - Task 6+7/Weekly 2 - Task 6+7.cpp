9ec6e9fd7fea11abd77f82f51bf0a1070e028fde
Kigaroth <hp_20@hotmail.com>
2020-09-14 20:05:53 +0200

    Add project files.

---
+++ Weekly 2 - Task 6+7/Weekly 2 - Task 6+7.cpp
@@ -0,0 +1,60 @@
+/* Weekly 2 - Task 6+7.cpp
+Make a program that asks the user what grade they want in Programming 1.
+If the answer is A, write "Outstanding".
+If the answer is B, write "Very good".
+If the answer is C, write "Good".
+If the answer is D, write "Some flaws".
+If the answer is E, write "Not very good...".
+If the answer is F, write "Fail".
+If the answer is something else, write "That is not a grade!". (Use switch - case)
+Update task 7 to accept both capital and lower case letters.Both 'A' and ‘a’, etc.are valid answers(use char as the variable type).
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+std::string grade;
+
+int main()
+{
+    std::cout << "\t\t\t\t Menu\n\n";
+
+    std::cout << "Please enter the grade you want in Programming 1:  ";
+    std::cin >> grade;
+    std::cout << std::endl;
+
+
+        switch ()
+
+}
+
+
+    //if (grade == "A"||"a") {
+    //    std::cout << "Outstanding.";
+    //}
+
+    //else if (grade == "B") {
+    //    std::cout << "Very good.";
+    //}
+
+    //else if (grade == "C") {
+    //    std::cout << "Good.";
+    //}
+
+    //else if (grade == "D") {
+    //    std::cout << "Some flaws.";
+    //}
+
+    //else if (grade == "E") {
+    //    std::cout << "Not very good...";
+    //}
+
+    //else if (grade == "F") {
+    //    std::cout << "Fail.";
+    //}
+
+    //else if (grade != "A" || "B" || "C" || "D" || "E" || "F") {
+    //    std::cout << "That is not a grade!";
+    //}
+
